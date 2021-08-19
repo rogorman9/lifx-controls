3eb21880a084a9c504f4c945c04146698f9ef395
Ryan O'Gorman <rogorman9@gmail.com>
2021-08-19 12:30:15 -0400

    Removes mock lights fallback

--- services/getAllLights.ts
+++ services/getAllLights.ts
@@ -4,11 +4,9 @@ import { Light } from '../types'
 import { authHeaders } from './common'
 
 export default async function getAllLights(): Promise<Light[] | undefined> {
-  if (LIFX_API_URL) {
     const response = await fetch(`${LIFX_API_URL}/lights/all`, {
       headers: authHeaders,
     })
     return response.json()
   }
-  return mockAllLights
 }
